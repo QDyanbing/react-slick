82ea8419b522bd407c512271d45492d8e9a707bf
高艳兵 <yanbing.gao@aliyun.com>
2026-06-25 10:27:19 +0800

    Memoize arrow components

--- src/arrows.js
+++ src/arrows.js
@@ -4,98 +4,102 @@ import React from "react";
 import { clsx } from "clsx";
 import { canGoNext } from "./utils/innerSliderUtils";
 
-export const PrevArrow = ({
-  currentSlide,
-  slideCount,
-  slidesToShow,
-  infinite,
-  prevArrow: customPrevArrow,
-  clickHandler
-}) => {
-  const disabled =
-    !infinite && (currentSlide === 0 || slideCount <= slidesToShow);
+export const PrevArrow = React.memo(
+  ({
+    currentSlide,
+    slideCount,
+    slidesToShow,
+    infinite,
+    prevArrow: customPrevArrow,
+    clickHandler
+  }) => {
+    const disabled =
+      !infinite && (currentSlide === 0 || slideCount <= slidesToShow);
 
-  const prevClasses = {
-    "slick-arrow": true,
-    "slick-prev": true,
-    "slick-disabled": disabled
-  };
+    const prevClasses = {
+      "slick-arrow": true,
+      "slick-prev": true,
+      "slick-disabled": disabled
+    };
 
-  const prevHandler = (e) => {
-    if (e) {
-      e.preventDefault();
+    const prevHandler = (e) => {
+      if (e) {
+        e.preventDefault();
+      }
+      clickHandler({ message: "previous" }, e);
+    };
+
+    const prevArrowProps = {
+      key: "0",
+      "data-role": "none",
+      className: clsx(prevClasses),
+      style: { display: "block" },
+      onClick: disabled ? null : prevHandler
+    };
+
+    const customProps = { currentSlide, slideCount };
+
+    if (customPrevArrow) {
+      return React.cloneElement(customPrevArrow, {
+        ...prevArrowProps,
+        ...customProps
+      });
     }
-    clickHandler({ message: "previous" }, e);
-  };
 
-  const prevArrowProps = {
-    key: "0",
-    "data-role": "none",
-    className: clsx(prevClasses),
-    style: { display: "block" },
-    onClick: disabled ? null : prevHandler
-  };
-
-  const customProps = { currentSlide, slideCount };
-
-  if (customPrevArrow) {
-    return React.cloneElement(customPrevArrow, {
-      ...prevArrowProps,
-      ...customProps
-    });
+    return (
+      <button key="0" type="button" {...prevArrowProps}>
+        {" "}
+        Previous
+      </button>
+    );
   }
+);
 
-  return (
-    <button key="0" type="button" {...prevArrowProps}>
-      {" "}
-      Previous
-    </button>
-  );
-};
+export const NextArrow = React.memo(
+  ({
+    currentSlide,
+    slideCount,
+    nextArrow: customNextArrow,
+    clickHandler,
+    ...props
+  }) => {
+    const disabled = !canGoNext({ ...props, currentSlide, slideCount });
 
-export const NextArrow = ({
-  currentSlide,
-  slideCount,
-  nextArrow: customNextArrow,
-  clickHandler,
-  ...props
-}) => {
-  const disabled = !canGoNext({ ...props, currentSlide, slideCount });
+    const nextClasses = {
+      "slick-arrow": true,
+      "slick-next": true,
+      "slick-disabled": disabled
+    };
 
-  const nextClasses = {
-    "slick-arrow": true,
-    "slick-next": true,
-    "slick-disabled": disabled
-  };
+    const nextHandler = (e) => {
+      if (e) {
+        e.preventDefault();
+      }
+      clickHandler({ message: "next" }, e);
+    };
 
-  const nextHandler = (e) => {
-    if (e) {
-      e.preventDefault();
+    const nextArrowProps = {
+      key: "1",
+      "data-role": "none",
+      className: clsx(nextClasses),
+      style: { display: "block" },
+      onClick: disabled ? null : nextHandler
+    };
+
+    const customProps = { currentSlide, slideCount };
+
+    if (customNextArrow) {
+      return React.cloneElement(customNextArrow, {
+        ...nextArrowProps,
+        ...customProps
+      });
     }
-    clickHandler({ message: "next" }, e);
-  };
 
-  const nextArrowProps = {
-    key: "1",
-    "data-role": "none",
-    className: clsx(nextClasses),
-    style: { display: "block" },
-    onClick: disabled ? null : nextHandler
-  };
-
-  const customProps = { currentSlide, slideCount };
-
-  if (customNextArrow) {
-    return React.cloneElement(customNextArrow, {
-      ...nextArrowProps,
-      ...customProps
-    });
+    return (
+      <button key="1" type="button" {...nextArrowProps}>
+        {" "}
+        Next
+      </button>
+    );
   }
-
-  return (
-    <button key="1" type="button" {...nextArrowProps}>
-      {" "}
-      Next
-    </button>
-  );
-};
+);
